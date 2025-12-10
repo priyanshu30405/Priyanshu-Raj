aac19bcc5ca2a4f0a879f24c020938a0fe081997
Priyanshu Raj <113039111+priyanshu30405@users.noreply.github.com>
2025-12-10 14:01:50 +0530

    Program3.java

--- Program3.java
+++ Program3.java
@@ -1,1 +1,19 @@
+class Program3 {
+    public static void main(String[] args) {
+        int a = 6;
 
+        int limit;
+        if (a % 2 == 0) {
+            limit = a - 1;
+        } else {
+            limit = a;
+        }
+
+        for (int i = 1; i <= limit; i += 2) {
+            System.out.print(i);
+            if (i + 2 <= limit) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
